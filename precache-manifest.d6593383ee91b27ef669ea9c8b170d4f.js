7e6424a9d928801c54dbd59b33ae9307a99870a8
Sahand Babali <42813816+sahandbabali@users.noreply.github.com>
2021-01-01 15:27:54 +0330

    Add files via upload

---
+++ precache-manifest.d6593383ee91b27ef669ea9c8b170d4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a76526196b6682870035",
+    "url": "/static/css/main.0251d062.chunk.css"
+  },
+  {
+    "revision": "a76526196b6682870035",
+    "url": "/static/js/main.a7652619.chunk.js"
+  },
+  {
+    "revision": "328073d5009f95ef2996",
+    "url": "/static/js/1.328073d5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12a0a479381f8ccd63555460bc29ff03",
+    "url": "/index.html"
+  }
+];
